d4679fadb46d258a4284ccdb260763fabf582acb
StephenMistele <34781493+StephenMistele@users.noreply.github.com>
2021-02-20 14:24:08 -0800

    axios

--- vue-notus-master/src/components/Cards/CardSearchHandler.vue
+++ vue-notus-master/src/components/Cards/CardSearchHandler.vue
@@ -2,29 +2,44 @@
   <div
     class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
   >
-  
-    <p>Query: {{query}}</p>
-    <p>Price: {{price}}</p>
-    <p>People: {{people}}</p>
-    <p>Time: {{time}}</p>
-
+    <button style="margin-top: 10px" v-on:click="queryDatabase()">
+      <p>Search</p>
+    </button>
+    <p>Query: {{ query }}</p>
+    <p>Price: {{ price }}</p>
+    <p>People: {{ people }}</p>
+    <p>Time: {{ time }}</p>
   </div>
 </template>
 <script>
+
 export default {
-    name: "CardSearchHandler",
+  name: "CardSearchHandler",
 
-    props: {
-        query: String,
-        price: Number,
-        people: Number,
-        time: Number,
+  props: {
+    query: String,
+    price: Number,
+    people: Number,
+    time: Number,
+  },
+
+  methods: {
+    async queryDatabase() {
+      //request for recipe
+      await axios
+        .request({
+          method: "GET",
+          url: `http://localhost:9078/api/products`,
+          dataType: "json",
+        })
+        .then((res) => {
+          console.log(res);
+          return res;
+        })
+        .catch((err) => {
+          console.log("api call failed", err);
+        });
     },
-
-    methods: {
-        queryDatabase() {
-
-        }
-    }
-}
+  },
+};
 </script>
