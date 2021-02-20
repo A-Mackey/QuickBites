73f5cdb5d75c993195c7f68a922e88b4699cf8d5
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2021-02-20 14:00:39 -0800

    Add basic search functionality

---
+++ vue-notus-master/src/components/Cards/CardSearchHandler.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+  >
+  
+    <p>Query: {{query}}</p>
+    <p>Price: {{price}}</p>
+    <p>People: {{people}}</p>
+    <p>Time: {{time}}</p>
+
+  </div>
+</template>
+<script>
+export default {
+    name: "CardSearchHandler",
+
+    props: {
+        query: String,
+        price: Number,
+        people: Number,
+        time: Number,
+    },
+
+    methods: {
+        queryDatabase() {
+
+        }
+    }
+}
+</script>
--- vue-notus-master/src/components/Cards/CardTable.vue
+++ vue-notus-master/src/components/Cards/CardTable.vue
@@ -1,11 +1,44 @@
 <template>
   <div>
-    <input v-model="message" placeholder="Search">
-    <p>{{message}}</p>
+
+    <div class="text-center">
+
+      <form
+        class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
+      >
+        <div class="relative flex w-full flex-wrap items-stretch">
+          <span
+            class="z-10 leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3"
+          >
+            <i class="fas fa-search"></i>
+          </span>
+          <input
+            type="text"
+            v-model="search"
+            placeholder="Find a Recipe..."
+            class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10"
+            style="margin-right: 100px"
+          />
+          <button style="margin-top: 10px;" v-on:click="callSearch">
+            <p>Search</p>
+          </button>
+        </div>
+      </form>
+
+    </div>
+      
+      
+      <p>Input: {{search}}</p>
+
+
+    <CardSearchHandler v-bind:query="searchQuery" v-bind:price="price" v-bind:people="people" v-bind:time="time"/>
+  
+  
   </div>
 </template>
 <script>
 //import TableDropdown from "@/components/Dropdowns/TableDropdown.vue";
+import CardSearchHandler from "@/components/Cards/CardSearchHandler.vue";
 
 import bootstrap from "@/assets/img/bootstrap.jpg";
 import angular from "@/assets/img/angular.jpg";
@@ -21,7 +54,11 @@ import team4 from "@/assets/img/team-4-470x470.png";
 export default {
   data() {
     return {
-      message: "",
+      search: "",
+      price: -1,
+      people: -1,
+      time: -1,
+      searchQuery: "",
       bootstrap,
       angular,
       sketch,
@@ -35,15 +72,44 @@ export default {
   },
   components: {
     //TableDropdown,
+    CardSearchHandler,
   },
+
   props: {
-    color: {
-      default: "light",
-      validator: function (value) {
-        // The value must match one of these strings
-        return ["light", "dark"].indexOf(value) !== -1;
-      },
-    },
+
   },
+
+  methods: {
+    callSearch() {
+      this.searchQuery = this.search;
+      this.price = 10;
+      this.people = 10;
+      this.time = 10;
+    }
+  }
 };
 </script>
+
+<style scoped>
+  .searchBarContainer {
+    vertical-align: middle;
+    background-color: teal;
+  }
+
+  .searchBar {
+    width: 80%;
+    height: 100px;
+    font-size: 70px;
+    border-radius: 50px;
+    border-color: transparent;
+  }
+
+  .searchButton {
+    position: absolute;
+    height: 90px;
+    width: 90px;
+    border-radius: 100%;
+    background-color: turquoise;
+  }
+  
+</style>
