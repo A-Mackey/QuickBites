109eafbdbae16d021cb25a657ca2bde311956986
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2021-02-21 00:00:02 -0800

    Update Landing.vue

--- vue-notus-master/src/views/Landing.vue
+++ vue-notus-master/src/views/Landing.vue
@@ -8,7 +8,7 @@
         <div
           class="absolute top-0 w-full h-full bg-center bg-cover"
           style="
-            background-image: url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80');
+            background-image: url('https://images.pexels.com/photos/4262002/pexels-photo-4262002.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
           "
         >
           <span
@@ -29,7 +29,7 @@
                   have a responsibility to 
                   uplift those around us in need.
                 </p>
-                
+
                 <p class="mt-4 text-lg text-gray-300">
                   There are many ways that you can help!
                 </p>
@@ -73,8 +73,8 @@
                   </div>
                   <h6 class="text-xl font-semibold">Post Affordable Recipes</h6>
                   <p class="mt-2 mb-4 text-gray-600">
-                    Divide details about your product or agency work into parts.
-                    A paragraph describing a feature will be enough.
+                    We appreciate the community that is being built here.
+                    We want this to be a place to find affordable and delicious home-made meals for all families.
                   </p>
                 </div>
               </div>
@@ -92,8 +92,7 @@
                   </div>
                   <h6 class="text-xl font-semibold">Shop Using Our Site</h6>
                   <p class="mt-2 mb-4 text-gray-600">
-                    Keep you user engaged by providing meaningful information.
-                    Remember that by this time, the user is curious.
+                    Using our site, you will be saving money, time, and be giving back to your community.
                   </p>
                 </div>
               </div>
@@ -111,82 +110,19 @@
                   </div>
                   <h6 class="text-xl font-semibold">Make a Donation</h6>
                   <p class="mt-2 mb-4 text-gray-600">
-                    Write a few lines about each one. A paragraph describing a
-                    feature will be enough. Keep you user engaged!
+                    All money will go directly into our benefits program fund to further discount family meals for qualifying low-income households.
                   </p>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="flex flex-wrap items-center mt-32">
-            <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
-              <div
-                class="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100"
-              >
-                <i class="fas fa-user-friends text-xl"></i>
-              </div>
-              <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                Working with us is a pleasure
-              </h3>
-              <p
-                class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700"
-              >
-                Don't let your uses guess by attaching tooltips and popoves to
-                any element. Just make sure you enable them first via
-                JavaScript.
-              </p>
-              <p
-                class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700"
-              >
-                The kit comes with three pre-built pages to help you get started
-                faster. You can change the text and images and you're good to
-                go. Just make sure you enable them first via JavaScript.
-              </p>
-              <router-link to="/" class="font-bold text-gray-800 mt-8">
-                Check Vue Notus!
-              </router-link>
-            </div>
-
-            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
-              <div
-                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-green-600"
-              >
-                <img
-                  alt="..."
-                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
-                  class="w-full align-middle rounded-t-lg"
-                />
-                <blockquote class="relative p-8 mb-4">
-                  <svg
-                    preserveAspectRatio="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 583 95"
-                    class="absolute left-0 w-full block h-95-px -top-94-px"
-                  >
-                    <polygon
-                      points="-30,95 583,95 583,65"
-                      class="text-green-600 fill-current"
-                    ></polygon>
-                  </svg>
-                  <h4 class="text-xl font-bold text-white">
-                    Top Notch Services
-                  </h4>
-                  <p class="text-md font-light mt-2 text-white">
-                    The Arctic Ocean freezes every winter and much of the
-                    sea-ice then thaws every summer, and that process will
-                    continue whatever happens.
-                  </p>
-                </blockquote>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
 
-      <section class="relative py-20">
+      <section class="relative py-10">
         <div
-          class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+          class=" top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
           style="transform: translateZ(0);"
         >
           <svg
@@ -207,9 +143,9 @@
 
       </section>
 
-      <section class="pt-20 pb-48">
+      <section class="mb-12">
         <div class="container mx-auto px-4">
-          <div class="flex flex-wrap justify-center text-center mb-24">
+          <div class="flex flex-wrap justify-center text-center mb-12">
             <div class="w-full lg:w-6/12 px-4">
               <h2 class="text-4xl font-semibold">Developers</h2>
             </div>
@@ -273,23 +209,9 @@
 import Navbar from "@/components/Navbars/AuthNavbar.vue";
 import FooterComponent from "@/components/Footers/Footer.vue";
 
-import team1 from "@/assets/img/team-1-800x800.jpg";
-import team2 from "@/assets/img/team-2-800x800.jpg";
-import team3 from "@/assets/img/team-3-800x800.jpg";
-import team4 from "@/assets/img/team-4-470x470.png";
-import team5 from "@/assets/img/team-4-470x470.png";
-import team6 from "@/assets/img/team-4-470x470.png";
-
 export default {
   data() {
     return {
-      team1,
-      team2,
-      team3,
-      team4,
-      team5,
-      team6,
-
       team: [
       [
         {
