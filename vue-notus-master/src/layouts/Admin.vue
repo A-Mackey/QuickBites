1a982dbcd83e0fe4458bf00316d4b9a5e9ef9f13
kevinrooney <kevinrooney135@gmail.com>
2021-02-21 04:06:08 -0800

    altered padding in saved

--- vue-notus-master/src/layouts/Admin.vue
+++ vue-notus-master/src/layouts/Admin.vue
@@ -4,11 +4,11 @@
     <div class="relative md:ml-64 mt-auto bg-gray-200">
       <admin-navbar />
       <!-- <header-stats /> -->
-      <div class="mt-auto w-full">
+      <div class="mt-auto">
     </div>
     </div>
     <div class="md:ml-64 bg-gray-200">
-      <div class="w-full">
+      <div class="">
         <router-view />
         <footer-admin />
       </div>
